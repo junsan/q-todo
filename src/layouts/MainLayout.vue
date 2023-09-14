7b19c2e5c9010432a384be0bd36f6c2f03728efe
Jun <xjunsantos@gmail.com>
2023-09-15 02:44:33 +0800

    Upload files

---
+++ src/layouts/MainLayout.vue
@@ -0,0 +1,87 @@
+<template>
+  <q-layout view="lHh Lpr lFf">
+    <q-header elevated>
+      <q-toolbar>
+        <q-btn
+          flat
+          dense
+          round
+          icon="menu"
+          aria-label="Menu"
+          @click="toggleLeftDrawer"
+        />
+
+        <q-toolbar-title>
+          Q-Todo
+        </q-toolbar-title>
+
+        <div>Q-Todo v1.0</div>
+      </q-toolbar>
+    </q-header>
+
+    <q-drawer
+      v-model="leftDrawerOpen"
+      show-if-above
+      bordered
+      style="background-color:  #1976d261; color: #333; padding-top: 50px;"
+    >
+      <q-list >
+        <EssentialLink
+          v-for="link in essentialLinks"
+          :key="link.title"
+          v-bind="link"
+        />
+      </q-list>
+    </q-drawer>
+
+    <q-page-container>
+      <router-view />
+    </q-page-container>
+  </q-layout>
+</template>
+
+<script>
+import { defineComponent, ref } from 'vue'
+import EssentialLink from 'components/EssentialLink.vue'
+
+const linksList = [
+  {
+    title: 'Home',
+    caption: 'quasar.dev',
+    icon: 'home',
+    link: '/#/'
+  },
+  {
+    title: 'About',
+    caption: 'github.com/quasarframework',
+    icon: 'description',
+    link: '/#/about'
+  },
+  {
+    title: 'Contact',
+    caption: 'chat.quasar.dev',
+    icon: 'smartphone',
+    link: '/#/contact'
+  }
+]
+
+export default defineComponent({
+  name: 'MainLayout',
+
+  components: {
+    EssentialLink
+  },
+
+  setup () {
+    const leftDrawerOpen = ref(false)
+
+    return {
+      essentialLinks: linksList,
+      leftDrawerOpen,
+      toggleLeftDrawer () {
+        leftDrawerOpen.value = !leftDrawerOpen.value
+      }
+    }
+  }
+})
+</script>
